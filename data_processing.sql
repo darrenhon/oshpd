3a6cda49ec4e733318d1fcfca993624ba769c7f6
darrenhon <darrenhonsoftware@gmail.com>
2015-12-18 23:17:53 -0800

    Fix data processing

--- data_processing.sql
+++ data_processing.sql
@@ -1,11 +1,11 @@
---combine all years into one table (takes around 40 mins)
-select * into darren_oshpd_allyears from decopdd_2009;
-insert into darren_oshpd_allyears select * from decopdd_2010;
-insert into darren_oshpd_allyears select * from decopdd_2011;
-insert into darren_oshpd_allyears select * from decopdd_2012;
+-- combine all years into one table (takes around 1 hr and 10 mins)
+-- the union elminates identical rows
+select * into darren_oshpd_allyears from decopdd_2009
+union select * from decopdd_2010
+union select * from decopdd_2011
+union select * from decopdd_2012
 -- 2013 has different column ordering from others. Need to explicitly name all columns
-insert into darren_oshpd_allyears (diag_p, odiag1, odiag2, odiag3, odiag4, odiag5, odiag6, odiag7, odiag8, odiag9, odiag10, odiag11, odiag12, odiag13, odiag14, odiag15, odiag16, odiag17, odiag18, odiag19, odiag20, odiag21, odiag22, odiag23, odiag24, ecode_p, ecode1, ecode2, ecode3, ecode4, proc_p, oproc1, oproc2, oproc3, oproc4, oproc5, oproc6, oproc7, oproc8, oproc9, oproc10, oproc11, oproc12, oproc13, oproc14, oproc15, oproc16, oproc17, oproc18, oproc19, oproc20, epoa_p, epoa1, epoa2, epoa3, epoa4, oshpd_id, typcare, agyradm, sex, ethncty, race, patzip, patcnty, los, los_adj, admtday, admtmth, admtyr, source, srcsite, srclicns, srcroute, admtype, disp, charge, rln, poa_p, proc_pdy, opoa1, opoa2, opoa3, opoa4, opoa5, opoa6, opoa7, opoa8, opoa9, opoa10, opoa11, opoa12, opoa13, opoa14, opoa15, opoa16, opoa17, opoa18, opoa19, opoa20, opoa21, opoa22, opoa23, opoa24, procdy1, procdy2, procdy3, procdy4, procdy5, procdy6, procdy7, procdy8, procdy9, procdy10, procdy11, procdy12, procdy13, procdy14, procdy15, procdy16, procdy17, procdy18, procdy19, procdy20, admtdate, dschdate, hplzip, qtr_adm, qtr_dsch, race_grp, sev_code, cat_code, grouper) 
-select diag_p, odiag1, odiag2, odiag3, odiag4, odiag5, odiag6, odiag7, odiag8, odiag9, odiag10, odiag11, odiag12, odiag13, odiag14, odiag15, odiag16, odiag17, odiag18, odiag19, odiag20, odiag21, odiag22, odiag23, odiag24, ecode_p, ecode1, ecode2, ecode3, ecode4, proc_p, oproc1, oproc2, oproc3, oproc4, oproc5, oproc6, oproc7, oproc8, oproc9, oproc10, oproc11, oproc12, oproc13, oproc14, oproc15, oproc16, oproc17, oproc18, oproc19, oproc20, epoa_p, epoa1, epoa2, epoa3, epoa4, oshpd_id, typcare, agyradm, sex, ethncty, race, patzip, patcnty, los, los_adj, admtday, admtmth, admtyr, source, srcsite, srclicns, srcroute, admtype, disp, charge, rln, poa_p, proc_pdy, opoa1, opoa2, opoa3, opoa4, opoa5, opoa6, opoa7, opoa8, opoa9, opoa10, opoa11, opoa12, opoa13, opoa14, opoa15, opoa16, opoa17, opoa18, opoa19, opoa20, opoa21, opoa22, opoa23, opoa24, procdy1, procdy2, procdy3, procdy4, procdy5, procdy6, procdy7, procdy8, procdy9, procdy10, procdy11, procdy12, procdy13, procdy14, procdy15, procdy16, procdy17, procdy18, procdy19, procdy20, admtdate, dschdate, hplzip, qtr_adm, qtr_dsch, race_grp, sev_code, cat_code, grouper from decopdd_2013;
+union select diag_p, odiag1, odiag2, odiag3, odiag4, odiag5, odiag6, odiag7, odiag8, odiag9, odiag10, odiag11, odiag12, odiag13, odiag14, odiag15, odiag16, odiag17, odiag18, odiag19, odiag20, odiag21, odiag22, odiag23, odiag24, ecode_p, ecode1, ecode2, ecode3, ecode4, proc_p, oproc1, oproc2, oproc3, oproc4, oproc5, oproc6, oproc7, oproc8, oproc9, oproc10, oproc11, oproc12, oproc13, oproc14, oproc15, oproc16, oproc17, oproc18, oproc19, oproc20, epoa_p, epoa1, epoa2, epoa3, epoa4, oshpd_id, typcare, agyradm, sex, ethncty, race, patzip, patcnty, los, los_adj, admtday, admtmth, admtyr, source, srcsite, srclicns, srcroute, admtype, disp, charge, rln, poa_p, proc_pdy, opoa1, opoa2, opoa3, opoa4, opoa5, opoa6, opoa7, opoa8, opoa9, opoa10, opoa11, opoa12, opoa13, opoa14, opoa15, opoa16, opoa17, opoa18, opoa19, opoa20, opoa21, opoa22, opoa23, opoa24, procdy1, procdy2, procdy3, procdy4, procdy5, procdy6, procdy7, procdy8, procdy9, procdy10, procdy11, procdy12, procdy13, procdy14, procdy15, procdy16, procdy17, procdy18, procdy19, procdy20, admtdate, dschdate, hplzip, qtr_adm, qtr_dsch, race_grp, sev_code, cat_code, grouper from decopdd_2013;
 
 -- create table to store icd9 codes for different cohort
 create table oshpd_cohort_icd9 (icd9 varchar(50) NOT NULL primary key, cohort varchar(50) NULL);
@@ -36,7 +36,24 @@ diag_p = icd9 or odiag1 = icd9 or odiag2 = icd9 or odiag3 = icd9 or odiag4 = icd
 -- delete the invalid rln
 delete from darren_chf_rln_distinct where rln = '---------';
 
--- select all admission history for rlns in CHF cohort
+-- select all admission history for rlns in CHF cohort (takes around 10 mins)
 select tbl1.* into darren_all_chf from darren_oshpd_allyears as tbl1 join darren_chf_rln_distinct as tbl2 on tbl1.rln = tbl2.rln;
 
+-- fix sex inconsistencies (takes around 35 mins)
+declare @rln varchar(50), @sex varchar(50)
+declare @male int, @female int;
+declare rlnCursor cursor for select rln from darren_all_chf group by rln having count(distinct sex) > 1
+  open rlnCursor
+    fetch rlnCursor into @rln
+    while @@FETCH_STATUS = 0
+    begin
+      select @male = sum(case when sex = '1' then 1 else 0 end), @female = sum(case when sex = '2' then 1 else 0 end) from darren_all_chf where rln = @rln
+      if @male = @female and @male = 0 set @sex = '3'
+      else if @female > @male set @sex = '2'
+      else set @sex = '1'
+      update darren_all_chf set sex = @sex where rln = @rln
+      fetch rlnCursor into @rln
+    end
+  close rlnCursor
+deallocate rlnCursor
 
