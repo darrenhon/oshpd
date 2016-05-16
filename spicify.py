2220a09318c1b551441971005b0ca383eed3a293
Darren <darrenhonsoftware@gmail.com>
2016-05-16 16:58:57 -0700

    Fix spicify sequence number count

--- spicify.py
+++ spicify.py
@@ -9,36 +9,25 @@ if len(sys.argv) > 3:
 fin = open(path, 'r')
 col = ocsv.getColumns(fin.readline().strip())
 syms = set()
-count = 0
-
-def countLineAndSym(line):
-  global count
-  count += 1
-  syms.add(line.strip().split(',')[col[target]])
-
-ocsv.runFunc(fin, countLineAndSym)
-fin.close()
-
-fout = open(out, 'w')
-dum = fout.write(str(count) + ' ' + str(len(syms)) + '\n')
-
-fin = open(path, 'r')
-dum = fin.readline()
-
 seqs = []
 pidToSeq = dict()
 def getSeqs(line):
   row = line.strip().split(',')
   pid = row[col['PID']]
+  sym = row[col[target]]
+  syms.add(sym)
   if pid in pidToSeq:
-    pidToSeq[pid].append(row[col[target]])
+    pidToSeq[pid].append(sym)
   else:
-    newSeq = [row[col[target]]]
+    newSeq = [sym]
     seqs.append(newSeq)
     pidToSeq[pid] = newSeq
 
 ocsv.runFunc(fin, getSeqs)
 fin.close()
+
+fout = open(out, 'w')
+dum = fout.write(str(len(seqs)) + ' ' + str(len(syms)) + '\n')
 
 for seq in seqs:
   fout.write(str(len(seq)) + ' ' + ' '.join(seq) + '\n')
