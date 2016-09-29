4db50fcf64f98be5156f8b53dfc54f7df4cd68ea
darrenhon <darrenhonsoftware@gmail.com>
2016-09-29 09:26:46 +0000

    Generalize sampling into arbitrary percentage

--- sampling.py
+++ sampling.py
@@ -3,8 +3,9 @@ import sys
 import random
 
 # argv[1] input file
-# argv[2] public output file
-# argv[3] private output file
+# argv[2] first output file
+# argv[3] second output file
+# argv[4] percentage of patients in the first output file
 
 # read input file the first round to load all PIDs
 fin = open(sys.argv[1], 'r')
@@ -13,24 +14,24 @@ pids = set()
 ocsv.runFunc(fin, lambda line: pids.add(line.strip().split(',')[col['PID']]))
 fin.close()
 
-# sample private PIDs
-pripids = set(random.sample(pids, int(len(pids) / 10)))
+# sample PIDs
+pids1 = set(random.sample(pids, int(len(pids)  * int(sys.argv[4]) / 100)))
 
-# read input file the second round to divide into public and private
-fpub = open(sys.argv[2], 'w')
-fpri = open(sys.argv[3], 'w')
+# read input file the second round to divide it
+f1 = open(sys.argv[2], 'w')
+f2 = open(sys.argv[3], 'w')
 fin = open(sys.argv[1], 'r')
 line = fin.readline()
-fpub.write(line)
-fpri.write(line)
+f1.write(line)
+f2.write(line)
 
 def write(line):
-  if line.strip().split(',')[col['PID']] in pripids:
-    fpri.write(line)
+  if line.strip().split(',')[col['PID']] in pids1:
+    f1.write(line)
   else:
-    fpub.write(line)
+    f2.write(line)
 
 ocsv.runFunc(fin, write)
 fin.close()
-fpri.close()
-fpub.close()
+f1.close()
+f2.close()
